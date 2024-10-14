288ac9940d583fc5bdf54eccbd681500dcf9a947
fabian boshoven <65905014+codefhacker@users.noreply.github.com>
2024-10-14 09:20:22 +0200

    Create Logica_lift.h

---
+++ src/Logica_lift.h
@@ -0,0 +1,24 @@
+#ifndef Logica_lift_H   // kijk of de libary al is gedefineert
+#define Logica_lift_H   // defineer de libary
+
+#include <Arduino.h>
+
+class Logica {
+  
+  public:
+    int verdieping[4] = {0,0,0,0};
+    
+
+    void berekenWaarNaarToe(int huidigeVerdieping, int gewensteVerdieping);
+
+    int berekenTotaleWaarde(int sensorBoven, int sensorBeneden, char sensorIR);
+    
+
+
+
+
+};
+
+
+
+#endif
