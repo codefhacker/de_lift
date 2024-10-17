4ff4ed861f948a7b71cc6e4cbbe8b298623c7e90
fabian boshoven <65905014+codefhacker@users.noreply.github.com>
2024-10-17 15:54:18 +0200

    Update main_master.ino

--- src/main_master.ino
+++ src/main_master.ino
@@ -1,61 +1,85 @@
-// Wire Master Writer
-// by Nicholas Zambetti <http://www.zambetti.com>
-
-// Demonstrates use of the Wire library
-// Writes data to an I2C/TWI slave device
-// Refer to the "Wire Slave Receiver" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
-
-
 #include <Wire.h>
-
+#include "IO_lift.h"
+#include "Logica_lift.h"
 int t;
-char c;
+int c;
+
+MotorLift MotorLift(13,12,11);
 void setup()
 
 {
+  MotorLift.setupMotor();
   Serial.begin(9600);
   Serial.println("test123");
   Wire.begin(); // join i2c bus (address optional for master)
 }
 
-byte x = 0;
+int x = 0;
 
 void loop()
 {
+  MotorLift.controlMotor(0);
   Wire.beginTransmission(4); // transmit to device #4
   
-  Wire.write("x is ");        // sends five bytes
+  //Wire.write("x is ");        // sends five bytes
   Wire.write(x);              // sends one byte  
   Wire.endTransmission();    // stop transmitting
+
+  // Wire.beginTransmission(2); // transmit to device #4
+  
+  // Wire.write("x is ");        // sends five bytes
+  // Wire.write(x);              // sends one byte  
+  // Wire.endTransmission();
+
+  Wire.beginTransmission(3); // transmit to device #4
+  
+  Wire.write("x is ");        // sends five bytes
+  Wire.write(x);              // sends one byte  
+  Wire.endTransmission();
+
+  // Wire.beginTransmission(4); // transmit to device #4
+  
+  // Wire.write("x is ");        // sends five bytes
+  // Wire.write(x);              // sends one byte  
+  // Wire.endTransmission();
+
+  // Wire.beginTransmission(5); // transmit to device #4
+  
+  // Wire.write("x is ");        // sends five bytes
+  // Wire.write(x);              // sends one byte  
+  // Wire.endTransmission();
 
   x++;
-  delay(10);
+  ReceiveSlave(2);
+  
   ReceiveSlave(4);
+  // ReceiveSlave(2);
+  // ReceiveSlave(3);
+  // ReceiveSlave(4);
+  // ReceiveSlave(5);
 
-  Wire.beginTransmission(5); // transmit to device #4
+  if (x >=9){
+    x = 0;
+  }
+
   
-  Wire.write("x is ");        // sends five bytes
-  Wire.write(x);              // sends one byte  
-  Wire.endTransmission();    // stop transmitting
-  delay(10);
-  ReceiveSlave(5);
   
 
-  char value[2];
+  
 }
 
 
 void ReceiveSlave(int slavenumer)  {
   Wire.requestFrom(slavenumer, 2);
-  int i = 0; //counter for each bite as it arrives
-  while (Wire.available()) {
-    c = Wire.read(); // every character that arrives it put in order in the empty array "t"
-    
+  
+  
+  c = Wire.read(); // every character that arrives it put in order in the empty array "t"
+  if (c == 1){
+    MotorLift.controlMotor(2);
   }
+  
+  Serial.print("recieved from : ");
+  Serial.println(slavenumer);
   Serial.println(c);   //shows the data in the array t
-  delay(10);
+  
 }
