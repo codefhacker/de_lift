b484add4d947cd60a7728eff1902bfd9d336b351
fabian boshoven <65905014+codefhacker@users.noreply.github.com>
2024-11-11 14:09:39 +0100

    Update main_master.ino

--- src/main_master.ino
+++ src/main_master.ino
@@ -1,85 +1,83 @@
-// #include <Wire.h>
-// #include "IO_lift.h"
-// #include "Logica_lift.h"
-// int t;
-// int c;
+#include <Wire.h>
+#include "IO_lift.h"
+#include "Logica_lift.h"
+#include <Keypad.h>
 
-// MotorLift MotorLift(13,12,11);
-// void setup()
+int t;
+int c;
+const byte ROWS = 4; 
+const byte COLS = 4; 
 
-// {
-//   MotorLift.setupMotor();
-//   Serial.begin(9600);
-//   Serial.println("test123");
-//   Wire.begin(); // join i2c bus (address optional for master)
-// }
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
 
-// int x = 0;
+byte rowPins[ROWS] = {22, 23, 24, 25}; 
+byte colPins[COLS] = {26, 27, 28, 29}; 
 
-// void loop()
-// {
-//   MotorLift.controlMotor(0);
-//   Wire.beginTransmission(4); // transmit to device #4
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+
+
+
+MotorLift MotorLift(13,12,11);
+void setup()
+
+{
+  MotorLift.setupMotor();
+  Serial.begin(9600);
+  Serial.println("test123");
+  Wire.begin(); // join i2c bus (address optional for master)
+}
+
+int x = 0;
+
+void loop()
+{
+  MotorLift.controlMotor(0);
+  char customKey = customKeypad.getKey();
+  if (customKey){
+    Serial.println(customKey);
+  }
+
+  for(int i = 1; i <=6; i++){
+    Wire.beginTransmission(i); // transmit to device #4
+    Wire.write(x);              // sends one byte  
+    Wire.endTransmission();    // stop transmitting
+  }
+  x++;
+
+  for (int i = 1; i <= 1; i++){
+    ReceiveSlave(i);
+  }
   
-//   //Wire.write("x is ");        // sends five bytes
-//   Wire.write(x);              // sends one byte  
-//   Wire.endTransmission();    // stop transmitting
-
-//   // Wire.beginTransmission(2); // transmit to device #4
   
-//   // Wire.write("x is ");        // sends five bytes
-//   // Wire.write(x);              // sends one byte  
-//   // Wire.endTransmission();
-
-//   Wire.beginTransmission(3); // transmit to device #4
-  
-//   Wire.write("x is ");        // sends five bytes
-//   Wire.write(x);              // sends one byte  
-//   Wire.endTransmission();
-
-//   // Wire.beginTransmission(4); // transmit to device #4
-  
-//   // Wire.write("x is ");        // sends five bytes
-//   // Wire.write(x);              // sends one byte  
-//   // Wire.endTransmission();
-
-//   // Wire.beginTransmission(5); // transmit to device #4
-  
-//   // Wire.write("x is ");        // sends five bytes
-//   // Wire.write(x);              // sends one byte  
-//   // Wire.endTransmission();
-
-//   x++;
-//   ReceiveSlave(2);
-  
-//   ReceiveSlave(4);
-//   // ReceiveSlave(2);
-//   // ReceiveSlave(3);
-//   // ReceiveSlave(4);
-//   // ReceiveSlave(5);
-
-//   if (x >=9){
-//     x = 0;
-//   }
+  //delay(1000);
+  if (x >=9){
+    x = 0;
+  }
 
   
   
 
   
-// }
+}
 
 
-// void ReceiveSlave(int slavenumer)  {
-//   Wire.requestFrom(slavenumer, 2);
+void ReceiveSlave(int slavenumer)  {
+  Wire.requestFrom(slavenumer, 2);
   
   
-//   c = Wire.read(); // every character that arrives it put in order in the empty array "t"
-//   if (c == 1){
-//     MotorLift.controlMotor(2);
-//   }
+  c = Wire.read(); // every character that arrives it put in order in the empty array "t"
+  if (c == 1){
+    MotorLift.controlMotor(2);
+  }
   
-//   Serial.print("recieved from : ");
-//   Serial.println(slavenumer);
-//   Serial.println(c);   //shows the data in the array t
+  // Serial.print("recieved from : ");
+  // Serial.println(slavenumer);
+  // Serial.println(c);   //shows the data in the array t
   
-// }
+}
