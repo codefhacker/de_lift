b3832490fb7f8e4ea0294f1a2aff97c26ccf3719
fabian boshoven <65905014+codefhacker@users.noreply.github.com>
2024-11-13 16:06:19 +0100

    Update main_master.ino

--- src/main_master.ino
+++ src/main_master.ino
@@ -10,20 +10,20 @@ const byte COLS = 4;
 
 Logica Logica;
 
-SegmentDisplay SegmentDisplay(8, 12, 11); // latchpin , 
+SegmentDisplay SegmentDisplay(8, 12, 11); // display pinnnen
 LiftKnop LiftKnop1(6, 9); // knoppin , ledpin
 
 
 
-char hexaKeys[ROWS][COLS] = {
+char hexaKeys[ROWS][COLS] = { // defineren van keypad knoppen
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
 
-byte rowPins[ROWS] = {22, 23, 24, 25}; 
-byte colPins[COLS] = {26, 27, 28, 29}; 
+byte rowPins[ROWS] = {22, 23, 24, 25}; // rij 1 keypad
+byte colPins[COLS] = {26, 27, 28, 29}; // rij 2 keypad
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
@@ -35,17 +35,12 @@ void setup()
 
 {
   LiftKnop1.setupPins();
+  pinMode(9,OUTPUT);
+  digitalWrite(9, HIGH);
   SegmentDisplay.setupSegmentDisplay();
   MotorLift.setupMotor();
   Serial.begin(9600);
-  Serial.println("test123");
-  SegmentDisplay.writeNumber(2); 
-  delay(1000);
-  SegmentDisplay.writeNumber(3); 
-  delay(1000);
-  SegmentDisplay.writeNumber(4); 
-  delay(1000);
-  
+ 
   Wire.begin(); // join i2c bus (address optional for master)
 
 }
@@ -61,11 +56,36 @@ void loop()
   int statusKopBoven = LiftKnop1.knopState;
   if(statusKopBoven){
     while(1){
-      MotorLift.controlMotor(0);
-      delay(10000000000);
       
-    }
+      MotorLift.controlMotor(0);
+      
+
+      for(int i = 1; i <=5; i++) {
+        Wire.beginTransmission(i); // transmit to device #4
+        Wire.write(10);
+        SegmentDisplay.writeNumber(10);  
+        digitalWrite(9, HIGH);            
+        Wire.endTransmission();
+        delay(100);    // stop transmitting
+      }
+      
+      for(int i = 1; i <=5; i++){
+
+      
+        Wire.beginTransmission(i); // transmit to device #4
+        Wire.write(11);
+        SegmentDisplay.writeNumber(11);
+        digitalWrite(9, LOW);              // sends one byte  
+        Wire.endTransmission();
+        delay(100);    // stop transmitting
+        
+      }
+        
+
   }
+  }  
+  
+  
   
 
   for(int i = 1; i <=6; i++){
@@ -91,75 +111,41 @@ void loop()
 
 
 void ReceiveSlave(int slavenummer)  {
-  
-  Wire.requestFrom(slavenummer, 2);
-  
-  
-  c = Wire.read(); // every character that arrives it put in order in the empty array "t"
- 
-  
-  
-  if (c == 2 || c == 4){
-    Logica.berekenWaarNaarToe(Logica.huidigeVerdieping,slavenummer - 1);
-    MotorLift.controlMotor(Logica.motorRichting);
-    Logica.slavenummer = slavenummer - 1;
-  }
-
-  if (c == 1){
+    Wire.requestFrom(slavenummer, 2);
+    c = Wire.read(); // Leest de inkomende data van de slave
     
-    Logica.huidigeVerdieping = slavenummer - 1;
-    Serial.println(Logica.huidigeVerdieping);
-    Logica.berekenWaarNaarToe(Logica.huidigeVerdieping,Logica.slavenummer);
-    MotorLift.controlMotor(Logica.motorRichting);
-    //Serial.println("sensor detecteerd");
-    //Serial.print("c :");
-    
-    
-    //Serial.print("verieping :");
-    Serial.print("slave :");
-    Serial.println(slavenummer -1);
-    SegmentDisplay.writeNumber(slavenummer -1);
-
-    if (Logica.motorRichting == 0){
-      char customKey = customKeypad.getKey();
-
-      if (customKey == '0'){
-        Logica.slavenummer = 0;
-
-      }
-      if (customKey == '1'){
-        Logica.slavenummer = 1;
-
-      }
-      if (customKey == '2'){
-        Logica.slavenummer = 2;
-
-      }
-      if (customKey == '2'){
-        Logica.slavenummer = 2;
-
-      }
-      if (customKey == '3'){
-        Logica.slavenummer = 3;
-
-      }
-      if (customKey == '4'){
-        Logica.slavenummer = 4;
-
-      }
+    if (c == 2 || c == 4) {
+        // Voeg bestemming toe en stuur lift naar die verdieping
+        Logica.berekenWaarNaarToe(Logica.huidigeVerdieping, slavenummer - 1);
+        MotorLift.controlMotor(Logica.motorRichting);
+        Logica.slavenummer = slavenummer - 1;
     }
-    Serial.print("array waarde : ");
-    Serial.println(Logica.verdieping[slavenummer -1]);
-    
 
-    
-  
-  }
+    if (c == 1) {
+        // Werk huidige verdieping bij en verwerk verdere bestellingen
+        Logica.huidigeVerdieping = slavenummer - 1;
+        Serial.println(Logica.huidigeVerdieping);
+        Logica.berekenWaarNaarToe(Logica.huidigeVerdieping, Logica.slavenummer);
+        MotorLift.controlMotor(Logica.motorRichting);
+        Serial.print("Slave: ");
+        Serial.println(slavenummer - 1);
+        SegmentDisplay.writeNumber(slavenummer - 1);
 
-  
-  
-  //Serial.println(c);
+        // Zodra de lift stilstaat, accepteer nieuwe toetseninput
+        if (Logica.motorRichting == 0) {
+            char customKey = customKeypad.getKey();
+
+            // Voeg verdieping toe aan wachtrij afhankelijk van toetseninput
+            if (customKey >= '0' && customKey <= '4') {
+                Logica.slavenummer = customKey - '0';
+            }
+        }
+
+        Serial.print("Array waarde: ");
+        Serial.println(Logica.verdieping[slavenummer - 1]);
+    }
 }
+
   
 
   
